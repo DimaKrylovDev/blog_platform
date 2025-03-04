e376e3929b8b6649a4de2c30ea1c0cf3d298397c
DimaKrylov <tumbo4kadima@yandex.ru>
2025-03-04 23:25:19 +0300

    added main

---
+++ backend/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+import uvicorn
+from db.base import async_session_maker, engine
+from endpoints import user_endpoint, blog_endpoint, auth
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from db.base import Base
+from db.models.user import User
+from db.models.role import Role
+from db.models.blog import Blog
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up...")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    
+    yield
+    
+    print("Shutting down...")
+    await engine.dispose()
+    
+app = FastAPI(title="Blog", lifespan=lifespan)
+
+app.include_router(auth.router, prefix = "/auth", tags=["auth"])
+
+app.include_router(user_endpoint.router, prefix="/user", tags=["users"])
+
+app.include_router(blog_endpoint.router, prefix="/blog", tags=["blog"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
+    
