10a39ad60400e5dbf5ca009401f28581c1ed98e5
DimaKrylov <tumbo4kadima@yandex.ru>
2025-03-17 23:42:11 +0300

    Fix bugs

--- backend/endpoints/auth.py
+++ backend/endpoints/auth.py
@@ -1,19 +1,22 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from repository.user import UserRepository
+from fastapi import APIRouter, HTTPException, status,Response
 from schemas.token import Token
-from .depends import get_user_repository
-from core.security import verify_password, create_acces_token, authenticate_user
-from fastapi.security import OAuth2PasswordRequestForm
-from pydantic import EmailStr
+from core.security import create_acces_token, authenticate_user
+from schemas.user import SUserLogin
+
 router = APIRouter()
 
 @router.post("/login", response_model=Token)
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await authenticate_user(form_data.username, form_data.password)
-
+async def login(users: SUserLogin, response: Response):
+    user = await authenticate_user(email=users.email, password=users.password)
+    
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
     
+    acces_token = create_acces_token({"sub": users.email})
+    response.set_cookie(key="access_jwt_token", value=acces_token, httponly=True)
+    
     return Token(
-        access_token= create_acces_token({"sub": user.email}),
+        access_token= acces_token,
         token_type="Bearer")
+    
+    
