11e549cd95ecc1c1aa7a2026af75fbd7261964fd
DimaKrylov <tumbo4kadima@yandex.ru>
2025-03-18 14:32:51 +0300

    added gitignore and removed backend

--- backend/repository/user.py
+++
@@ -1,8 +0,0 @@
-from .base import BaseRepository
-from db.models.user import User
-from schemas.user import SUser
-
-
-class UserRepository(BaseRepository):
-    model = User
-    model_pydantic_schema = SUser
--- backend/schemas/token.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    
-class Login(BaseModel):
-    email: EmailStr | None = None
-    password: str    
