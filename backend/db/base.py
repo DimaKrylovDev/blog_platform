4960c86b5dacb7f8a2ee294be58c11efd65e3b55
DimaKrylov <tumbo4kadima@yandex.ru>
2025-03-22 22:17:44 +0300

    added redis connection

--- backend/db/base.py
+++ backend/db/base.py
@@ -3,12 +3,16 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from core.config import settings
 from sqlalchemy.orm import DeclarativeBase
 from databases import Database
+from redis.client import Redis 
+from core.config import settings
 
 database = Database(settings.POSTGRES_CLEAR_URL)
 clear_engine = create_engine(settings.POSTGRES_CLEAR_URL)
 engine = create_async_engine(settings.POSTGRES_URL)
 
 async_session_maker = async_sessionmaker(engine, class_ = AsyncSession, expire_on_commit=False)
+
+redis_session = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
 class Base(DeclarativeBase):
     metadata = MetaData()
