296f3a9ac3a8de47a81adc7018696f7f91c6d70f
DimaKrylov <tumbo4kadima@yandex.ru>
2025-04-05 01:44:04 +0300

    fixed bugs

--- backend/endpoints/depends.py
+++ backend/endpoints/depends.py
@@ -1,10 +1,13 @@
 from repository.user import UserRepository 
 from repository.blog import BlogRepository
+from repository.comments import CommentRepository
+from repository.likes import LikeRepository
 from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from core.security import decode_acces_token
 from jose import JWTError
+
 
 
 def get_user_repository() -> UserRepository:
@@ -13,7 +16,13 @@ def get_user_repository() -> UserRepository:
 def get_blog_repository() -> BlogRepository:
     return BlogRepository()
 
-def get_current_user(token: Annotated[str, Depends(OAuth2PasswordBearer(tokenUrl="token"))]):
+def get_comment_repository() -> CommentRepository:
+    return CommentRepository()
+
+def get_like_repository() -> LikeRepository:
+    return LikeRepository()
+
+async def get_current_user(token: Annotated[str, Depends(OAuth2PasswordBearer(tokenUrl="/auth/login"))]):
     credentional_exeptions = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -21,16 +30,20 @@ def get_current_user(token: Annotated[str, Depends(OAuth2PasswordBearer(tokenUrl
     )
     try:
         payload = decode_acces_token(token)
-        if payload is None:
+        print(payload)
+        if not payload:
             raise credentional_exeptions
-        user_email = payload.get("sub")
-        if user_email is None:
+        username = payload.get('sub')
+        print(username)
+        if username is None:
             raise credentional_exeptions
+        user = await UserRepository.get_one_or_none(name=username)
+        print(user)
+        if user is None:
+            raise credentional_exeptions
+    
+        return user
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="user not found")
     
-    user = UserRepository.get_by_email(email_=user_email)
-    if user is None:
-        raise credentional_exeptions
-    
-    return user
+
