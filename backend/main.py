0cc3e88a773dadca34c1e424f911b0c57c8f5671
DimaKrylov <tumbo4kadima@yandex.ru>
2025-04-05 01:44:30 +0300

    fixed bugs

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
 from db.base import async_session_maker, engine
-from endpoints import user_endpoint, blog_endpoint, auth
+from endpoints import user_endpoint, blog_endpoint, comments, auth, likes
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from db.base import Base
 from db.models.user import User
-from db.models.role import Role
+from db.models.roles import Role
 from db.models.blog import Blog
 
 @asynccontextmanager
@@ -22,12 +22,15 @@ async def lifespan(app: FastAPI):
     
 app = FastAPI(title="Blog", lifespan=lifespan)
 
+app.include_router(likes.router, prefix="/like", tags=["likes"])
+
 app.include_router(auth.router, prefix = "/auth", tags=["auth"])
 
 app.include_router(user_endpoint.router, prefix="/user", tags=["users"])
 
 app.include_router(blog_endpoint.router, prefix="/blog", tags=["blog"])
 
+app.include_router(comments.router, prefix="/comments", tags=["comments"])
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
