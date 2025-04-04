1237be17211298d5c0447552490615c00df05d5c
DimaKrylov <tumbo4kadima@yandex.ru>
2025-04-05 01:45:12 +0300

    fixed bugs

--- backend/schemas/blog.py
+++ backend/schemas/blog.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
+from typing import List
 import datetime
 
 class SBlog(BaseModel):
     id: int
     tittle: str
     information: str
+    likes:int = 0
     created_at: datetime.datetime
 
 class SBlogCreate(BaseModel):
